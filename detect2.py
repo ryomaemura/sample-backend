920b977a7a59b0b27870824c049337b705bc8ff9
ryomaemura <ryomaemura34@gmail.com>
2023-07-16 17:27:23 +0900

    add detect2.py

---
+++ detect2.py
@@ -0,0 +1,14 @@
+import uvicorn  # uvicornのインポートを追加
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+# オプション(host=0.0.0.0とし、EC2の外部IPアドレスを指定してアクセス可能にする)
+if __name__ == "__main__":
+    uvicorn.run(app, host="54.249.43.163", port=8002)
