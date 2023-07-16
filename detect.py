ba20beb1b2973e11e92f85657ea06981af3d5251
ryomaemura <ryomaemura34@gmail.com>
2023-07-16 17:23:36 +0900

    fix port

--- detect.py
+++ detect.py
@@ -11,4 +11,4 @@ async def root():
 
 # オプション(host=0.0.0.0とし、EC2の外部IPアドレスを指定してアクセス可能にする)
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
